e012172a87515aa13619c2ce7c5038e8aadc1dff
Lucas <lucas.bravi.2008@gmail.com>
2021-03-05 01:26:16 -0300

    Created Database and Frontend

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
